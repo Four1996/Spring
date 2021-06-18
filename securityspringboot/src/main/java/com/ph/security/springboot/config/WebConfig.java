a4ec266f7f2233995be97a5f74c3f9704d4e93c2
panhao <phnobugs@gmail.com>
2021-06-18 22:23:01 +0800

    更新自定义login页面 更新数据库依赖

--- securityspringboot/src/main/java/com/ph/security/springboot/config/WebConfig.java
+++ securityspringboot/src/main/java/com/ph/security/springboot/config/WebConfig.java
@@ -22,7 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
     // 默认Url根路径跳转到/login,此url为spring security提供
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("redirect:/login");
+        registry.addViewController("/").setViewName("redirect:/login-view");
+        registry.addViewController("/login-view").setViewName("login");
     }
 
 }
--- securityspringboot/src/main/java/com/ph/security/springboot/config/WebSecurityConfig.java
+++ securityspringboot/src/main/java/com/ph/security/springboot/config/WebSecurityConfig.java
@@ -44,13 +44,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // 安全拦截机制(最重要)
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                // .csrf().disable()//屏蔽CSRF控制，即spring security不再限制CSRF
+                .authorizeRequests()
+
                 .antMatchers("/r/r1").hasAuthority("p1")
                 .antMatchers("/r/r2").hasAuthority("p2")
                 .antMatchers("/r/**").authenticated()//所有/r/**的请求必须认证通过
                 .anyRequest().permitAll()//除了/r/**，其他的请求可以访问
                 .and()
                 .formLogin()//允许表单登录
+                .loginPage("/login-view")//登录页面
+                .loginProcessingUrl("/login")
                 .successForwardUrl("/login-success");//自定义登录成功给的页面地址
 
     }
